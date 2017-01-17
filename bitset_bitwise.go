886421d5ae9ba463d35bd8d4f8bb79313afd6a92
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-01-18 08:52:36 +1030

    Add bitwise range functions

--- bitset_bitwise.go
+++ bitset_bitwise.go
@@ -26,3 +26,113 @@ func (b Bitset) Difference(b1, b2 Bitset) {
 func (b Bitset) SymmetricDifference(b1, b2 Bitset) {
 	bitwise.XOR(b, b1, b2)
 }
+
+func (b Bitset) ComplementRange(b1 Bitset, start, end uint) {
+	if start > end {
+		panic(errEndLessThanStart)
+	}
+
+	if end > b.Len() || end > b1.Len() {
+		panic(errOutOfRange)
+	}
+
+	if mask := mask1(start, end); mask != 0 {
+		b[start>>3] = b[start>>3]&^mask | (^b1[start>>3])&mask
+	}
+
+	start = (start + 7) &^ 7
+	bitwise.Not(b[start>>3:end>>3], b1[start>>3:end>>3])
+
+	if mask := mask2(end); mask != 0 {
+		end &^= 7
+		b[end>>3] = b[end>>3]&^mask | (^b1[end>>3])&mask
+	}
+}
+
+func (b Bitset) UnionRange(b1, b2 Bitset, start, end uint) {
+	if start > end {
+		panic(errEndLessThanStart)
+	}
+
+	if end > b.Len() || end > b1.Len() || end > b2.Len() {
+		panic(errOutOfRange)
+	}
+
+	if mask := mask1(start, end); mask != 0 {
+		b[start>>3] = b[start>>3]&^mask | (b1[start>>3]|b2[start>>3])&mask
+	}
+
+	start = (start + 7) &^ 7
+	bitwise.Or(b[start>>3:end>>3], b1[start>>3:end>>3], b2[start>>3:end>>3])
+
+	if mask := mask2(end); mask != 0 {
+		end &^= 7
+		b[end>>3] = b[end>>3]&^mask | (b1[end>>3]|b2[end>>3])&mask
+	}
+}
+
+func (b Bitset) IntersectionRange(b1, b2 Bitset, start, end uint) {
+	if start > end {
+		panic(errEndLessThanStart)
+	}
+
+	if end > b.Len() || end > b1.Len() || end > b2.Len() {
+		panic(errOutOfRange)
+	}
+
+	if mask := mask1(start, end); mask != 0 {
+		b[start>>3] = b[start>>3]&^mask | (b1[start>>3]&b2[start>>3])&mask
+	}
+
+	start = (start + 7) &^ 7
+	bitwise.And(b[start>>3:end>>3], b1[start>>3:end>>3], b2[start>>3:end>>3])
+
+	if mask := mask2(end); mask != 0 {
+		end &^= 7
+		b[end>>3] = b[end>>3]&^mask | (b1[end>>3]&b2[end>>3])&mask
+	}
+}
+
+func (b Bitset) DifferenceRange(b1, b2 Bitset, start, end uint) {
+	if start > end {
+		panic(errEndLessThanStart)
+	}
+
+	if end > b.Len() || end > b1.Len() || end > b2.Len() {
+		panic(errOutOfRange)
+	}
+
+	if mask := mask1(start, end); mask != 0 {
+		b[start>>3] = b[start>>3]&^mask | (b1[start>>3]&^b2[start>>3])&mask
+	}
+
+	start = (start + 7) &^ 7
+	bitwise.AndNot(b[start>>3:end>>3], b1[start>>3:end>>3], b2[start>>3:end>>3])
+
+	if mask := mask2(end); mask != 0 {
+		end &^= 7
+		b[end>>3] = b[end>>3]&^mask | (b1[end>>3]&^b2[end>>3])&mask
+	}
+}
+
+func (b Bitset) SymmetricDifferenceRange(b1, b2 Bitset, start, end uint) {
+	if start > end {
+		panic(errEndLessThanStart)
+	}
+
+	if end > b.Len() || end > b1.Len() || end > b2.Len() {
+		panic(errOutOfRange)
+	}
+
+	if mask := mask1(start, end); mask != 0 {
+		b[start>>3] = b[start>>3]&^mask | (b1[start>>3]^b2[start>>3])&mask
+	}
+
+	start = (start + 7) &^ 7
+	bitwise.XOR(b[start>>3:end>>3], b1[start>>3:end>>3], b2[start>>3:end>>3])
+
+	if mask := mask2(end); mask != 0 {
+		end &^= 7
+		b[end>>3] = b[end>>3]&^mask | (b1[end>>3]^b2[end>>3])&mask
+	}
+}
