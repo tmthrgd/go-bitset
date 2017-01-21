d6658c08d1e359ea6cfe52fb9e0299cf6e313547
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-01-21 17:23:52 +1030

    Add tests for Union, Intersection, Difference, SymmetricDifference

--- bitset_bitwise_test.go
+++ bitset_bitwise_test.go
@@ -21,3 +21,75 @@ func TestComplement(t *testing.T) {
 		t.Error("Complement failed, None should have returned true")
 	}
 }
+
+func TestUnion(t *testing.T) {
+	b, b1 := New(80), New(80)
+
+	b.SetRange(0, 40)
+	b1.SetRange(40, 80)
+
+	b.Union(b, b1)
+
+	if !b.All() {
+		t.Error("Union failed")
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	b, b1 := New(80), New(80)
+
+	b.SetAll()
+	b1.SetRange(40, 80)
+
+	b.Intersection(b, b1)
+
+	if !b.IsRangeClear(0, 40) {
+		t.Error("Intersection failed")
+	}
+
+	if !b.IsRangeSet(40, 80) {
+		t.Error("Intersection failed")
+	}
+}
+
+func TestDifference(t *testing.T) {
+	b, b1 := New(80), New(80)
+
+	b.SetAll()
+	b1.SetRange(40, 80)
+
+	b.Difference(b, b1)
+
+	if !b.IsRangeSet(0, 40) {
+		t.Error("Difference failed")
+	}
+
+	if !b.IsRangeClear(40, 80) {
+		t.Error("Difference failed")
+	}
+}
+
+func TestSymmetricDifference(t *testing.T) {
+	b, b1 := New(80), New(80)
+
+	b.SetRange(20, 60)
+	b1.SetRange(40, 80)
+
+	b.SymmetricDifference(b, b1)
+
+	if !b.IsRangeClear(0, 20) {
+		t.Error("SymmetricDifference failed")
+	}
+
+	if !b.IsRangeSet(20, 40) {
+		t.Error("SymmetricDifference failed")
+	}
+
+	if !b.IsRangeClear(40, 60) {
+		t.Error("SymmetricDifference failed")
+	}
+
+	if !b.IsRangeSet(60, 80) {
+		t.Error("SymmetricDifference failed")
+	}
+}
